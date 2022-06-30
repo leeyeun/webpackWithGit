477ae90658bb2cb09f2d26d28b32b552a8b1a0cf
leeyeun <dpdms0109@naver.com>
2022-06-30 13:14:02 +0900

    first commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import { plus } from "./plus.js";
+import "./style.css";
+import tiger from "./tiger.png";
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.innerHTML = `<img src = "${tiger}">`;
+});
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+    mode: "development",
+    entry: {
+        main: path.resolve("./src/app.js"),
+    },
+    output: {
+        // publicPath: "/webpack/dist/",
+        filename: "[name].js",
+        path: path.resolve("./dist"),
+    },
+    module: {
+        rules: [
+            // {
+            //     test: /\.js$/,
+            //     use: [path.resolve("./myLoader.js")],
+            // },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|jpg|jped|gif|svg)$/,
+                type: "asset",
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 200 * 1024, // 1kb가 1024byte 이기 때문에 20kb를 원한다면 1024에 20을 곱합니다.
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        new webpack.BannerPlugin({
+            banner:
+                "마지막 빌드 시간은 " + new Date().toLocaleString() + "입니다.",
+        }),
+    ],
+};
